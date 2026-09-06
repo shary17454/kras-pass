c36205665fdd8faf60033690416c1c14d8200ac5
shary17454 <sharyalhwaid@gmail.com>
2026-09-06 19:14:32 +0300

    Point review submission to build 77

--- tools/submit_app_store_review.py
+++ tools/submit_app_store_review.py
@@ -19,7 +19,7 @@ ISSUER_ID = "26cc2279-524d-4d33-ba75-9333cf111ad1"
 KEY_ID = "668Z2T3Q47"
 APP_ID = os.environ.get("APP_STORE_APP_ID", "6801506973")
 VERSION_STRING = os.environ.get("APP_STORE_VERSION", "1.1.4")
-BUILD_NUMBER = os.environ.get("APP_STORE_BUILD", "61")
+BUILD_NUMBER = os.environ.get("APP_STORE_BUILD", "77")
 
 WHATS_NEW_AR = (
     "إضافة ألعاب وأنماط جديدة، وتحسين قتال الجليد والماء، ورفع جودة الرسومات، "
